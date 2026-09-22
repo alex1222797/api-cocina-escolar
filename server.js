3d59abc37cf176c7b750397c75aaf8251af1c9a3
Alex Garcia <mauricioalexandergarciacornejo@gmail.com>
2026-09-21 20:51:21 -0600

    agregar otro endpoint a mi api desarrollada con node

--- server.js
+++ server.js
@@ -512,6 +512,62 @@ app.get("/informe" , async (req ,res) =>{
 });
 
 
+/**INFORME HISTORICO POR FECHA */
+app.get("/informe/:fecha" , async (req , res) => {
+    const fecha =req.params.fecha.trim();
+
+    //definir la aplicacion de fecha 
+    const formatoFecha = /^\d{4}-\d{2}-\d{2}$/;
+
+    if (!formatoFecha.test(fecha)){
+        return res.status(400).json({
+            status: "error",
+            mensaje: "La fecha debe tener el formato YYY-MM-DD"
+        });
+    }
+
+    try{
+        const [informe] = await pool.query(
+            `SELECT 
+            u.tipo,
+            COUNT(m.id) AS entregados,
+            COUNT(
+                CASE 
+                    WHEN m.fecha_devolucion IS NOT NULL
+                    THEN 1
+                END
+            ) AS devueltos,
+
+            COUNT(
+                CASE
+                    WHEN a.id IS NOT NULL
+                    AND m.fecha_devolucion IS NULL
+                    THEN 1
+                END
+            ) AS pendientes
+
+        FROM utensilios AS u
+        LEFT JOIN movimientos AS m
+            ON m.utencilio_id = u.id
+        AND DATE(m.fecha_retiro) = ?
+
+        GROUP BY u.id, u.tipo
+        ORDER BY u.tipo ASC
+            `,
+            [fecha]
+        );
+
+        return res.status(200).json(informe);
+    }catch (error){
+        console.error ("Error generando informe historico:" , error);
+
+        return res.status(500).json({
+            status: "error",
+            mensaje: " No se pudo generar el informe historico"
+        });
+    }
+});
+
 /**registro de retiro mediante codigo de barras */
 app.post("/retiro/codigo" , async (req , res) => {
     try {
